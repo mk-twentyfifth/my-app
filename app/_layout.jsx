43b1bc3ecc87045523d812252473339cc8dbceee
mk-helloworlds <mengkheangnuthpro2@gmail.com>
2025-09-11 01:15:58 +0700

    feat: #9 finished - tabs & tabs.screen - dashboard layout route group

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -20,6 +20,7 @@ const RootLayout = () => {
 
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
 
+        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
       </Stack>
     </>
   );
