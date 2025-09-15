035719f372c1ab5f71290a9d6df951b2aff06148
mk-helloworlds <mengkheangnuthpro2@gmail.com>
2025-09-16 01:43:30 +0700

    feat #20 finished - Activity Indicator - loading screen with laoding component

--- components/auth/GuestOnly.jsx
+++ components/auth/GuestOnly.jsx
@@ -1,7 +1,7 @@
 import { useRouter } from "expo-router";
 import { useUser } from "../../hooks/useUser";
 import { useEffect } from "react";
-import { Text } from "react-native";
+import ThemedLoader from "../ThemedLoader";
 
 const GuestOnly = ({ children }) => {
   const { user, authChecked } = useUser();
@@ -14,7 +14,7 @@ const GuestOnly = ({ children }) => {
   }, [user, authChecked]);
 
   if (!authChecked || user) {
-    return <Text>Loading</Text>;
+    return <ThemedLoader />;
   }
 
   return children;
